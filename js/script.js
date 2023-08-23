aaa439567cd4a73a61c26d7a7c5df5fd7e0fd220
AssadullahADELYAR <103497017+AssadullahADELYAR@users.noreply.github.com>
2023-08-23 14:31:15 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,6 @@
-const sideMenu = document.querySelector(".nav-item");
-
-function openMenu() {
-  sideMenu.style.right = "0";
-}
-
-function closeMenu() {
-  sideMenu.style.right = "-350rem";
-}
-
 const btnNav = document.querySelector(".btn-mobile-nav");
 const header = document.querySelector(".header");
 
 btnNav.addEventListener("click", () => {
   header.classList.toggle("nav-open");
 });
-console.log(sideMenu);
-
-console.log("hello console");
